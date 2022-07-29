be27830626069a1983f3a977f5310eb892025a4c
Ben Schwartz <Ben.Schwartz@nsm-seating.com>
2022-07-29 12:32:25 -0500

    main

--- main.py
+++ main.py
@@ -4,37 +4,32 @@ import json
 import pandas as pd 
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.engine import URL
-from sqlalchemy import create_engine
 
-def connect():
+def generate_Dates():
+    # now = datetime.now()
 
-    #Arguments for connection
-    # DRIVER_NAME = 'ODBC Driver 17 for SQL Server'
-    DRIVER_NAME = 'SQL Server'
-    SERVER_NAME = 'SQLDATA'
-    DATABASE_NAME = 'BCSReports'
+    # day = now.day
+    # month = now.month
+    # year = now.year
 
-    
-    #Connecting paramaters
-    connection_string = f"""
-        DRIVER={{{DRIVER_NAME}}};
-        SERVER={SERVER_NAME};            DATABASE={DATABASE_NAME};
-        Trusted_Connection = yes;
-        uid = WNSM\ben.schwartz;
-        pwd = Dish929292Intern;
-    """
+    # if day < 10:
+    #     day = "0" + str(day)
 
-    connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-    
-    engine = create_engine(connection_url)
-    
-    return engine
+    # if month < 10:
+    #     month = "0" + str(month)
+        
+   
+     
+    # today = str(month)  + "-" + str(day) + "-" + str(year)
+    # from_date = today + " 00:00:00"
+    # to_date  = today + " 23:59:59"
 
-def get_groupNames():
-    print(connect())
+    from_date = "07-27-2022 00:00:00"
+    to_date = "07-27-2022 23:59:59"
+    return [from_date, to_date]
+
 
 #Works with the .env file and helps to keep the API key secure (A matching .env file will have to be setup on any machine running this code)
 #The purpose of a .env file is to hide any environment variables that contain information that needs to be hidden api_keys, passwords... etc
@@ -42,53 +37,73 @@ def get_groupNames():
 def configure():
     load_dotenv()   #This function is from the dotenv library
 
-
 #fetches data using azugas TRIP api call 
 #Returns the data in JSON format
 #https://developer.azuga.com/docs/activity <---- Documentation found here
 def request_trips():
     
+    dates = generate_Dates()
     #Provided URL for this call
-    # url = 'https://api.azuga.com/azuga-ws/v1/activity/list.json'
+    url = 'https://api.azuga.com/azuga-ws/v1/activity/list.json'
 
-    url =  'https://api.azuga.com/azuga-ws/v1/trip/info.json'
     #Required headers (specified in documentation)
     #Key has to encoded (Encoded key is found in .env file)
     headers = {'Authorization': 'Basic {}'.format(os.getenv('encoded_key'))}
 
     #Parameters required for the call
     parameter = {
-        # 'groupName': '125',
-        'fromDate': '1656693871000', #Start date and time for desired range (UTC)
-        'toDate': '1657125871000'    #End date and time for desired range (UTC)
+        'allGroupsReport': 'true',
+        'from': dates[0], #Start date and time for desired range (UTC)
+        'to': dates[1],    #End date and time for desired range (UTC)
     }
+
     #Returns a response object that contains all the data from the call 
     response = requests.get(url, headers=headers, params = parameter)
-    print(response.text)
-    #Return the data in JSON format
+
     return json.loads(response.text)
+
+def get_all_trips(pages):
+    for page in range(pages):
+        dates = generate_Dates()
+        #Provided URL for this call
+        url = 'https://api.azuga.com/azuga-ws/v1/activity/list.json'
+
+        #Required headers (specified in documentation)
+        #Key has to encoded (Encoded key is found in .env file)
+        headers = {'Authorization': 'Basic {}'.format(os.getenv('encoded_key'))}
+
+        #Parameters required for the call
+        parameter = {
+            'allGroupsReport': 'true',
+            'from': dates[0], #Start date and time for desired range (UTC)
+            'to': dates[1],    #End date and time for desired range (UTC)
+            'page': page
+        }
+
+        #Returns a response object that contains all the data from the call 
+        response = requests.get(url, headers=headers, params = parameter)
+        json_data = json.loads(response.text)
+        df = pd.DataFrame(json_data['activityResponseVO']['activityReport'])
+        df.to_csv("data_July.csv", mode = 'a', index = False, header= False)
 
 
 def main():
 
-    # #ensure configure runs before anything else 
-    # configure()
+    #ensure configure runs before anything else 
+    configure()
 
-
-    # #Turn JSON data into a dicionary to handle data easier
-    # json_dict =  request_trips()
-
-    # print(json_dict['tripInfoFeed']['trips'])
-
-    get_groupNames()
+    #Turn JSON data into a dicionary to handle data easier
+    json_dict =  request_trips()
+    #print(json_dict)
 
     #Turn data into a pandas dataFrame
-    # df = pd.DataFrame(json_dict['activityResponseVO']['activityReport'])
-
+    df = pd.DataFrame(json_dict['activityResponseVO']['activityReport'])
     #Write the data to a csv
-    # df.to_csv("test.csv", index = False)
+    df.to_csv("data_July.csv", mode = 'a', index = False, header= False)
+
+    total_pages = (json_dict['activityResponseVO']['totalPages'])
+    get_all_trips(total_pages)
 
 #Driver code
 main()
-
 
